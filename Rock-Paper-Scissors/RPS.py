72ff894ad7101b2276ea2e29662dfceabee6f3fe
JBreitenbr <119942939+JBreitenbr@users.noreply.github.com>
2023-05-04 22:08:47 +0200

    Create RPS.py

---
+++ Rock-Paper-Scissors/RPS.py
@@ -0,0 +1,35 @@
+import random as rnd
+rnd.seed(149)
+guess=[""]
+o_guess=[""]
+w=["R","P","S"]
+ord={}
+resp={"R":"P","P":"S","S":"R"}
+cnt=[0]
+def markov4(history, play_order):
+    if "".join(history[-4:]) in play_order.keys():
+        play_order["".join(history[-4:])] += 1
+    else:
+        play_order["".join(history[-4:])] = 1
+    possible = ["".join(history[-(4 - 1) :]) + k for k in ["R", "P", "S"]]
+    for pm in possible:
+        if not pm in play_order.keys():
+            play_order[pm] = 0
+    predict = max(possible, key=lambda key: play_order[key])
+    return predict[-1]
+
+
+def player(prev_play,opponent_history=[]):
+    if prev_play == "" or cnt[0]>69:
+      prev_play=rnd.choice(w)
+      cnt[0]=0
+    hist=opponent_history
+    hist.append(prev_play)
+    cnt[0]+=1
+    if len(hist) >= 4:
+        p=resp[markov4(hist,ord)]
+        guess[0]=p
+    if guess[0]=="":
+      guess[0]=rnd.choice(w)
+    return guess[0]
+
